c6aef4ac0cfb712778c94d3dedaf298cad8f71f8
romanovich3313 <124934750+romanovich3313@users.noreply.github.com>
2023-04-30 19:15:18 +0300

    up_bot

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 USERS = {}
 
 
-# decorator
 def error_handler(func):
     def inner(*args):
         try:
@@ -21,79 +20,77 @@ def hello_user():
     return "How can I help you?"
 
 
-def unknown_command(_):
-    return "unknown_command"
-
-
-def close_bot(_):
-    return exit("Good bye")
+@error_handler
+def add_contact(name, phone):
+    USERS[name] = phone
+    return f"User {name} added"
 
 
 @error_handler
-def add_user(args):
-    name, phone = args
-    USERS[name] = phone
-    return f'User {name} added!'
+def change_phone(name, new_phone):
+    if name in USERS:
+        old_phone = USERS[name]
+        USERS[name] = new_phone
+        return f'User {name} has new phone number: {new_phone}, old phone number: {old_phone}'
+    else:
+        return f'This user: {name} is not in your phone book'
 
 
 @error_handler
-def change_phone(args):
-    name, phone = args
-    old_phone = USERS[name]
-    USERS[name] = phone
-    return f'У {name} тепер телефон: {phone} Старий номер: {old_phone}'
+def show_phone(name):
+    return f'Phone number for {name}: {USERS[name]}'
 
 
-def show_all(_):
+def show_all():
     result = ''
     for name, phone in USERS.items():
         result += f'Name: {name}, phone: {phone}\n'
-    return result[:-1]
+    return result
 
 
-@error_handler
-def show_phone(args):
-    name, phone = args
-    USERS[name] = phone
-    return f'Phone number for {name} : {USERS[name]}'
+def unknown_command(command):
+    return f"Not command {command}"
+
+
+def close_bot():
+    exit("Good bye")
 
 
 HANDLERS = {
-    'hello': hello_user,
-    'add': add_user,
-    'change': change_phone,
-    'show all': show_all,
+    'add': add_contact,
     'phone': show_phone,
+    'all': show_all,
+    'change': change_phone,
     'exit': close_bot,
     'goodbye': close_bot,
     'close': close_bot,
 }
 
 
-def parse_input(user_input):
-    command, *args = user_input.split()
-    command = command.lstrip()
-
-    try:
-        handler = HANDLERS[command.lower()]
-    except KeyError:
-        if args:
-            command = command + ' ' + args[0]
-            args = args[1:]
-        handler = HANDLERS.get(command.lower(), unknown_command)
-    return handler, args
+def parse_command(command):
+    parts = command.split()
+    command_name = parts[0]
+    if len(parts) > 1:
+        command_args = parts[1:]
+    else:
+        command_args = []
+    return command_name, command_args
 
 
 def main():
+    print(hello_user())
     while True:
-        user_input = input('Please enter command and args: ')
-        handler, *args = parse_input(user_input)
-        result = handler(*args)
-        if not result:
-            print('goodbye')
-            break
-        print(result)
+        command_line = input('Please enter command and args: ').strip()
+        command_name, command_args = parse_command(command_line)
+        if command_name in HANDLERS:
+            try:
+                result = HANDLERS[command_name](*command_args)
+                print(result)
+            except TypeError:
+                print('Invalid input. Please try again.')
+        else:
+            print(unknown_command(command_name))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
